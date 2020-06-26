8e98f327656fdb1df2d91dcc08c8e28f9aa89741
LeSnake04 <47243761+LeSnake04@users.noreply.github.com>
2020-06-26 17:04:12 +0200

    Add files via upload

---
+++ define.js
@@ -0,0 +1,82 @@
+let distros = {
+	"endeavouros" : {
+		"specs" : 2,
+		"consolesxp": 4,
+		"generalexp": 4,
+		"setup": 4,
+		"usages":{
+			"creative":4,
+			"gaming":4,
+			"office":3,
+			"browsing":3,
+			"code":4
+		},
+		"guis":["xfce","i3","cinnamon","plasma","gnome","budgie","deepin"]
+	}
+}
+
+let guis = {
+	"xfce" : {
+		"customizeable":5,
+		"generalexp":3,
+		"consoleexp":4,
+		"win":4,
+		"mac":2,
+		"coding":4,
+		"office":4,
+		"specs":2
+	},
+	"i3" : {
+		"customizeable":5,
+		"generalexp":5,
+		"consoleexp":5,
+		"win":1,
+		"mac":1,
+		"coding":5,
+		"office":2,
+		"specs":1
+	},
+	"cinnemon" : {
+		"customizeable":4,
+		"generalexp":2,
+		"consoleexp":3,
+		"win":1,
+		"mac":1,
+		"coding":5,
+		"office":2
+	}
+}
+
+let guis = {
+
+}
+
+let questions = {
+	"previusos" : {
+		"de_q":"Welches Betriebsystem hast du vorher Benutzt",
+		"de_win":"Windows",
+		"de_mac":"MacOS"
+	},
+	"specs" : {
+		"de_q":"Wie gut ist dein rechner(oder Laptop)",
+		"de_1":"uralter Toaster",
+		"de_2":"Älterer Office PC",
+		"de_3":"Standard Office PC",
+		"de_4":"Moderner Rechnner",
+		"de_5":"High end PC"
+	},
+	"consolesexp":{
+		"de_q":"Bist du mit der Konsole vertraut?",
+    "de_1":"Nein, ich weiß nicht was das ist.",
+   	"de_2":"Nein, aber ich habe ein paar Befehle kopiert",
+    "de_3":"Ja, ich habe mich dran gewöhnt mit ihr zu arbeiten und kenne einige Befehle.",
+    "de_4":"Ich nutze sie nicht selten",
+    "de_5":"Die Konsole ist mein Zuhause, ich komme bestens zurecht!"
+	},
+	"usages":{
+
+	},
+	"generalexp":{
+
+	}
+}
